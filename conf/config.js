88770ebd8403d282ff72974eec2d282ad0429324
krunalprajapati0997 <krunalprajapati22121999@gmail.com>
2022-04-15 17:24:38 +0530

    book

--- conf/config.js
+++ conf/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    serviceID: "VA9cb51b694da39b9ac1cea3eecf126818",
+    serviceID: "VA61b28308e5a1bf9398a0f955a47e020b",
     accountID: "ACa1919d2e8d810f59d5c7005ffc7facc8",
     // authToken: "360fbd6b3868528afd014d9f066c2a6b",
     authToken: "023d89619c55c6fc1968e1aac2269f50",
